1c90240f3ffcf49171eac255ea43577e039a25c7
Reid Larson <reid.d.larson@gmail.com>
2019-10-24 09:35:42 -0400

    Site SQL queries set up

---
+++ 09_Capstone/Capstone/DAL/SiteSqlDAO.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Capstone.Models;
+
+namespace Capstone.DAL
+{
+    public class SiteSqlDAO : ISiteDAO
+    {
+        private string connectionString;
+        public SiteSqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Site GetSiteById(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM site WHERE site_id = @id", connection);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    reader.Read();
+                    return SqlToSite(reader);
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Site> GetSitesInCampground(Campground campground)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM site WHERE campground_id = @id", connection);
+                    cmd.Parameters.AddWithValue("@id", campground.Id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Site> sites = new List<Site>();
+                    while (reader.Read()){
+                        sites.Add(SqlToSite(reader));
+                    }
+                    return sites;
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Site SqlToSite(SqlDataReader reader)
+        {
+            return new Site(
+                Convert.ToInt32(reader["site_id"]),
+                Convert.ToInt32(reader["campground_id"]),
+                Convert.ToInt32(reader["site_number"]),
+                Convert.ToInt32(reader["max_occupancy"]),
+                Convert.ToBoolean(reader["accessible"]),
+                Convert.ToInt32(reader["max_rv_length"]),
+                Convert.ToBoolean(reader["utilities"])
+                );
+        }
+    }
+}
--- 09_Capstone/Capstone/Models/Site.cs
+++ 09_Capstone/Capstone/Models/Site.cs
@@ -6,6 +6,17 @@ namespace Capstone.Models
 {
     public class Site
     {
+        public Site(int id, int campgroundId, int siteNumber, int maxOccupancy, bool accessible, int maxRVLength, bool utilities)
+        {
+            Id = id;
+            CampgroundId = campgroundId;
+            SiteNumber = siteNumber;
+            MaxOccupancy = maxOccupancy;
+            Accessible = accessible;
+            MaxRVLength = maxRVLength;
+            Utilities = utilities;
+        }
+
         public int Id { get; }
         public int CampgroundId { get; }
         public int SiteNumber { get; }
@@ -17,7 +28,7 @@ namespace Capstone.Models
 
         public Reservation CreateReservation(DateTime fromDate, DateTime toDate, string name)
         {
-            
+            // TODO Make sure reservation time is available
 
             Reservation reservation = new Reservation(Id, name, fromDate, toDate, DateTime.Now);
 
