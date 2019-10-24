78840b06fa9e5c29229ea4fbcedc378b2b49208e
Reid Larson <reid.d.larson@gmail.com>
2019-10-24 14:28:43 -0400

    MainMenu display works

--- 09_Capstone/Capstone/DAL/ISiteDAO.cs
+++ 09_Capstone/Capstone/DAL/ISiteDAO.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Capstone.DAL
 {
-    interface ISiteDAO
+    public interface ISiteDAO
     {
         List<Site> GetSitesInCampground(Campground campground);
 
---
+++ 09_Capstone/Capstone/Views/CLIMenu.cs
@@ -0,0 +1,207 @@
+﻿using Capstone.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Views
+{
+    public abstract class CLIMenu
+    {
+        /*** 
+         * Model Data that this menu system needs to operate on goes here.
+         ***/
+
+        /// <summary>
+        /// This is where every sub-menu puts its options for display to the user.
+        /// </summary>
+        protected Dictionary<string, string> menuOptions;
+
+        /// <summary>
+        /// The Title of this menu
+        /// </summary>
+        public string Title { get; set; }
+
+        protected IParkDAO parkDAO;
+        protected ICampgroundDAO campgroundDAO;
+        protected ISiteDAO siteDAO;
+        protected IReservationDAO reservationDAO;
+
+        /// <summary>
+        /// Constructor - pass in model data here
+        /// </summary>
+        public CLIMenu(IParkDAO parkDAO, ICampgroundDAO campgroundDAO, ISiteDAO siteDAO, IReservationDAO reservationDAO)
+        {
+            menuOptions = new Dictionary<string, string>();
+            this.parkDAO = parkDAO;
+            this.campgroundDAO = campgroundDAO;
+            this.siteDAO = siteDAO;
+            this.reservationDAO = reservationDAO;
+        }
+
+        /// <summary>
+        /// Run starts the menu loop
+        /// </summary>
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine(Title);
+                DrawMenuOptions();
+                string choice = GetString(@"Select a command: ").ToUpper();
+
+                if (menuOptions.ContainsKey(choice))
+                {
+                    if (choice == "Q")
+                    {
+                        break;
+                    }
+                    if (!ExecuteSelection(choice))
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Pause("Invalid selection.");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Draws the defined menu options on screen.
+        /// </summary>
+        protected void DrawMenuOptions()
+        {
+            foreach (KeyValuePair<string, string> menuItem in menuOptions)
+            {
+                Console.WriteLine($"{menuItem.Key}) {menuItem.Value}");
+            }
+        }
+        
+       
+        /// <summary>
+        /// Given a valid menu selection, runs the approriate code to do what the user is asking for.
+        /// </summary>
+        /// <param name="choice">The menu option (key) selected by the user</param>
+        /// <returns>True to keep executing the menu (loop), False to exit this menu (break)</returns>
+        abstract protected bool ExecuteSelection(string choice);
+        
+        #region User Input Helper Methods
+        /// <summary>
+        /// This continually prompts the user until they enter a valid integer.
+        /// </summary>
+        /// <param name="message">The string to prompt the user with</param>
+        /// <returns>A valid integer entered by the user</returns>
+        protected int GetInteger(string message)
+        {
+            int resultValue = 0;
+            while (true)
+            {
+                Console.Write(message + " ");
+                string userInput = Console.ReadLine().Trim();
+                if (int.TryParse(userInput, out resultValue))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("!!! Invalid input. Please enter a valid whole number.");
+                }
+            }
+            return resultValue;
+        }
+
+        /// <summary>
+        /// This continually prompts the user until they enter a valid double.
+        /// </summary>
+        /// <param name="message">The string to prompt the user with</param>
+        /// <returns>A valid double entered by the user</returns>
+        protected double GetDouble(string message)
+        {
+            double resultValue = 0;
+            while (true)
+            {
+                Console.Write(message + " ");
+                string userInput = Console.ReadLine().Trim();
+                if (double.TryParse(userInput, out resultValue))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("!!! Invalid input. Please enter a valid decimal number.");
+                }
+            }
+            return resultValue;
+        }
+
+        /// <summary>
+        /// This continually prompts the user until they enter a valid bool.
+        /// </summary>
+        /// <param name="message">The string to prompt the user with</param>
+        /// <returns>True or false.  The user can type Y or true for true values, N or false for false values.</returns>
+        protected bool GetBool(string message)
+        {
+            bool resultValue = false;
+            while (true)
+            {
+                Console.Write(message + " ");
+                string userInput = Console.ReadLine().Trim();
+                if (userInput.ToUpper() == "Y")
+                {
+                    resultValue = true;
+                    break;
+                }
+                else if (userInput == "N")
+                {
+                    resultValue = false;
+                    break;
+                }
+                else if (bool.TryParse(userInput, out resultValue))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("!!! Invalid input. Please enter [True, False, Y or N].");
+                }
+            }
+            return resultValue;
+        }
+
+        /// <summary>
+        /// This continually prompts the user until they enter a valid string (1 or more characters).
+        /// </summary>
+        /// <param name="message">The string to prompt the user with</param>
+        /// <returns>String entered by the user</returns>
+        protected string GetString(string message)
+        {
+            while (true)
+            {
+                Console.Write(message + " ");
+                string userInput = Console.ReadLine().Trim();
+                if (!String.IsNullOrEmpty(userInput))
+                {
+                    return userInput;
+                }
+                else
+                {
+                    Pause("!!! Invalid input. Please enter a valid menu option.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows a message to the user and waits for the user to hit return
+        /// </summary>
+        /// <param name="message">Displays a message to the user and then waits for them to hit Return.</param>
+        protected void Pause(string message)
+        {
+            Console.WriteLine(" " + message);
+            Console.Write(" Press any key to continue.");
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
